d24da48d4ea195478913ee2ce888443931a9e723
codingscode <heronnestg@hotmail.com>
2023-04-13 21:46:42 -0300

    parte 6

--- rntour/App.js
+++ rntour/App.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react'
-import { Button, Linking, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 
 
@@ -8,16 +8,20 @@ import { Button, Linking, StyleSheet, Text, View } from 'react-native'
 function App() {
    const [name, setName] = useState('Aladin')
    const [session, setSession] = useState({ number: 6, title: 'state' })
+   const [current, setCurrent] = useState(true)
+
 
    const onPressHandler = () => {
       setName('Programando com Mash')
       setSession({ number:7, title: 'Style' })
+      setCurrent(!current)
    }
 
    return (
       <View style={styles.body} >
          <Text style={styles.text} >{name}</Text>
          <Text style={styles.text} >This is session number {session.number} and about {session.title}</Text>
+         <Text style={styles.text} >{current ? 'current session' : 'next session'}</Text>
          <Button title='Update State' onPress={onPressHandler} ></Button>
       </View>
    )
