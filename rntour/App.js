bbc9a1f38e8c5c8511870416260e36d03c2705d9
codingscode <heronnestg@hotmail.com>
2023-04-14 22:04:43 -0300

    parte 20

--- rntour/App.js
+++ rntour/App.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
    body: {
       width: 400,
       height: 400,
-      flexDirection: 'row',
+      flexDirection: 'row-reverse',
       backgroundColor: 'yellow',
       alignItems: 'center',
       justifyContent: 'center',
