f1532c85d62d0717b548e7beae07b35a4a545d29
codingscode <heronnestg@hotmail.com>
2023-04-14 21:53:03 -0300

    parte 14

--- rntour/App.js
+++ rntour/App.js
@@ -15,10 +15,10 @@ function App() {
 
    return (
       <View style={styles.body} >
-         <Text style={styles.text} >{name}</Text>
-         <View style={styles.button} >
-            <Button title='Update State' onPress={onPressHandler} ></Button>
+         <View style={styles.view1} >
+            <Text style={styles.text} >1</Text>
          </View>
+         
       </View>
    )
 }
@@ -32,22 +32,21 @@ const styles = StyleSheet.create({
       borderWidth: 10,
       borderColor: 'green',
       borderRadius: 10,
-      //margin: 40
+   },
+   view1: {
+      width: 100,
+      height: 100,
+      backgroundColor: 'lightblue',
+      alignItems: 'center',
+      justifyContent: 'center'
    },
    text: {
       color: 'black',
       fontSize: 40,
       fontStyle: 'italic',
       margin: 10,
-      textTransform: 'uppercase'
    },
-   button: {
-      width: 150,
-      height: 60,
-      backgroundColor: 'orange'
-
-      
-   }
+   
    
 })
 
