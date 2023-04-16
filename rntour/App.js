07be568579334b006c7c232f6ff6063ee93f01b2
codingscode <heronnestg@hotmail.com>
2023-04-15 22:36:18 -0300

    parte 25

--- rntour/App.js
+++ rntour/App.js
@@ -1,30 +1,28 @@
 
 import React, { useState } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 
 
 
 function App() {
-   const [name, setName] = useState('Style Test')
+   const [Items, setItems] = useState([
+      { key: 1, item: 'Item 1' },
+      { key: 2, item: 'Item 2' },
+      { key: 3, item: 'Item 3' },
+      { key: 4, item: 'Item 4' },
+      { key: 5, item: 'Item 5' },
+      { key: 6, item: 'Item 6' },
+      { key: 7, item: 'Item 7' },
+      { key: 8, item: 'Item 8' }
+   ])
    
-
-   const onPressHandler = () => {
-      setName('STyle Test is Done!')
-   }
 
    return (
       <View style={styles.body} >
-         <View style={styles.view1} >
-            <Text style={styles.text} >1</Text>
+         <View style={styles.item} >
+            <Text style={styles.text} >Item 1</Text>
          </View>
-         <View style={styles.view2} >
-            <Text style={styles.text} >2</Text>
-         </View>
-         <View style={styles.view3} >
-            <Text style={styles.text} >3</Text>
-         </View>
-         
       </View>
    )
 }
@@ -32,10 +30,13 @@ function App() {
 const styles = StyleSheet.create({
    body: {
       flex: 1,
-      flexDirection: 'row', // column
+      flexDirection: 'column',
       backgroundColor: 'yellow',
-      alignItems: 'center',
-      justifyContent: 'flex-start', //space-between, flex-end, center, space-around
+   },
+   item: {
+      backgroundColor: 'orange',
+      justifyContent: 'center',
+      alignItems: 'center'
    },
    view1: {
       width: 100,
