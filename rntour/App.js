ea8d7f01a4483d7fdc43bbc5e777462ab93b7a44
codingscode <heronnestg@hotmail.com>
2023-04-16 22:55:41 -0300

    parte 33

--- rntour/App.js
+++ rntour/App.js
@@ -1,23 +1,23 @@
 
 import React, { useState } from 'react'
-import { RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 
 
 
 function App() {
    const [Items, setItems] = useState([
-      { key: 1, item: 'Item 1' },
-      { key: 2, item: 'Item 2' },
-      { key: 3, item: 'Item 3' },
-      { key: 4, item: 'Item 4' },
-      { key: 5, item: 'Item 5' },
-      { key: 6, item: 'Item 6' },
-      { key: 7, item: 'Item 7' },
-      { key: 8, item: 'Item 8' },
-      { key: 9, item: 'Item 9' },
-      { key: 10, item: 'Item 10' },
-      { key: 11, item: 'Item 11' }
+      { key: '1', name: 'Item 1' },
+      { key: '2', name: 'Item 2' },
+      { key: '3', name: 'Item 3' },
+      { key: '4', name: 'Item 4' },
+      { key: '5', name: 'Item 5' },
+      { key: '6', name: 'Item 6' },
+      { key: '7', name: 'Item 7' },
+      { key: '8', name: 'Item 8' },
+      { key: '9', name: 'Item 9' },
+      { key: '10', name: 'Item 10' },
+      { key: '11', name: 'Item 11' }
    ])
 
    const [Refreshing, setRefreshing] = useState(false)
@@ -29,14 +29,21 @@ function App() {
    }
 
    return (
-      <ScrollView refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['red']} />} style={styles.body} >
+      <FlatList data={Items}
+         renderItem={({item}, i) => (
+            <View style={styles.item} key={i} >
+               <Text style={styles.text} >{item.name}</Text>
+            </View>
+         )}
+       />
+      /* {/* <ScrollView refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['red']} />} style={styles.body} >
          {Items.map((item, i) => {
             return (
                <View style={styles.item} key={i} >
                   <Text style={styles.text} >{item.item}</Text>
                </View>
             )})}
-      </ScrollView>
+      </ScrollView> */
    )
 }
 
