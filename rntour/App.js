72c72f414ff19bd8d1c8485254ef15496e7314d3
codingscode <heronnestg@hotmail.com>
2023-04-19 22:33:32 -0300

    parte 40

--- rntour/App.js
+++ rntour/App.js
@@ -1,102 +1,41 @@
 
 import React, { useState } from 'react'
-import { FlatList, RefreshControl, ScrollView, SectionList, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 
 
 
 function App() {
-   const [Items, setItems] = useState([
-      { name: 'Item 1' },
-      { name: 'Item 2' },
-      { name: 'Item 3' },
-      { name: 'Item 4' },
-      { name: 'Item 5' },
-      { name: 'Item 6' },
-      { name: 'Item 7' },
-      { name: 'Item 8' },
-      { name: 'Item 9' },
-      { name: 'Item 10' },
-      { name: 'Item 11' }
-   ])
-
-   const DATA = [
-      {
-         title: 'Title 1',
-         data: ['Item 1-1', 'Item 1-2', 'Item 1-3']
-      },
-      {
-         title: 'Title 2',
-         data: ['Item 2-1', 'Item 2-2', 'Item 2-3']
-      },
-      {
-         title: 'Title 3',
-         data: ['Item 3-1']
-      },
-      {
-         title: 'Title 4',
-         data: ['Item 4-1', 'Item 4-2']
-      }
-   ]
-
-   const [Refreshing, setRefreshing] = useState(false)
-
-   const onRefresh = () => {
-      setRefreshing(true)
-      setItems([...Items, { name: 'Item 69'}])
-      setRefreshing(false)
-   }
+  
 
    return (
-      <SectionList sections={DATA}
-         keyExtractor={(item, index) => index.toString()}
-         renderItem={({ item }) => (
-            <Text style={styles.text} >{item}</Text>
-         )}
-         renderSectionHeader={({section})=> (
-            <View style={styles.item} >
-               <Text style={styles.text} >{section.title}</Text>
-            </View>
-         )}
-       />
-      /*<FlatList data={Items}
-         keyExtractor={(item, index) => index.toString()}
-         renderItem={({item}, i) => (
-            <View style={styles.item} >
-               <Text style={styles.text} >{item.name}</Text>
-            </View>
-         )}
-         refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['red']} />}
-       /> */
-      /* {/* <ScrollView refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['red']} />} style={styles.body} >
-         {Items.map((item, i) => {
-            return (
-               <View style={styles.item} key={i} >
-                  <Text style={styles.text} >{item.item}</Text>
-               </View>
-            )})}
-      </ScrollView> */
+      <View style={styles.body} >
+         <Text style={styles.text} >
+            Please write your name:
+         </Text>
+         <TextInput style={styles.input} placeholder='e.g. John' />
+      </View>
+      
    )
 }
 
 const styles = StyleSheet.create({
    body: {
       flex: 1,
-      flexDirection: 'column',
       backgroundColor: 'yellow',
-   },
-   item: {
-      margin: 10,
-      backgroundColor: 'orange',
-      justifyContent: 'center',
       alignItems: 'center'
    },
    text: {
       color: 'black',
-      fontSize: 40,
-      fontStyle: 'italic',
+      fontSize: 20,
       margin: 10,
    },
+   input: {
+      width: 200,
+      borderWidth: 1,
+      borderColor: '#555',
+      borderRadius: 5
+   }
    
    
 })
