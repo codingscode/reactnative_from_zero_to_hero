5977a01e5697ffa88fd9a3f1fa7dfafbe076b194
codingscode <heronnestg@hotmail.com>
2023-04-25 22:51:16 -0300

    parte 94

--- rntour/src/App.js
+++ rntour/src/App.js
@@ -1,16 +1,31 @@
 
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Pressable, Modal, Image, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { enableScreens } from 'react-native-screens'
 
+enableScreens()
 
 function App() {
    
-   
+   const Stack = createStackNavigator()
+
+   const ScreenA = () => {
+      return (
+         <View>
+            <Text>
+               Screen A
+            </Text>
+         </View>
+      )
+   }
 
    return (
       <NavigationContainer>
-
+         <Stack.Navigator>
+            <Stack.Screen name="Screen_A" component={ScreenA} />
+         </Stack.Navigator>
       </NavigationContainer>      
    )
       
