685979351dd8cf94157c088d5e3518f03c8ec2d6
codingscode <heronnestg@hotmail.com>
2023-04-27 21:36:50 -0300

    parte 110

--- rntour/src/App.js
+++ rntour/src/App.js
@@ -25,24 +25,25 @@ function App() {
             if (route.name === 'Screen_A') {
                iconName = 'autoprefixer'
                size = focused ? 25 : 20
-               color = focused ? '#ff8080' : '#00e6e6'
+               //color = focused ? '#ff8080' : '#00e6e6'
             }
             else if (route.name === 'Screen_B') {
                iconName = 'btc'
                size = focused ? 25 : 20
-               color = focused ? '#00e6ac' : '#cc00cc'
+               //color = focused ? '#00e6ac' : '#cc00cc'
             }
             return (
                <FontAwesome5 name={iconName} size={size} color={color} />
             )
          }
+         
       }
    }
    
    
    return (
-         <NavigationContainer>
-         <Tab.Navigator screenOptions={handler} >
+      <NavigationContainer>
+         <Tab.Navigator screenOptions={handler} tabBarOptions={{ activeTintColor: '#f0f' }} >
             <Tab.Screen name="Screen_A" component={ScreenA}  />
             <Tab.Screen name="Screen_B" component={ScreenB} />
          </Tab.Navigator>
