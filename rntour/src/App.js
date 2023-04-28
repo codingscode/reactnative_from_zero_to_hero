576ec8528270a2b979e272832070e492c4017d18
codingscode <heronnestg@hotmail.com>
2023-04-27 21:21:34 -0300

    parte 108

--- rntour/src/App.js
+++ rntour/src/App.js
@@ -24,12 +24,14 @@ function App() {
             let iconName
             if (route.name === 'Screen_A') {
                iconName = 'autoprefixer'
+               size = focused ? 25 : 20
             }
             else if (route.name === 'Screen_B') {
                iconName = 'btc'
+               size = focused ? 25 : 20
             }
             return (
-               <FontAwesome5 name={iconName} />
+               <FontAwesome5 name={iconName} size={size} />
             )
          }
       }
@@ -51,71 +53,3 @@ function App() {
 export default App
 
 
-/* const ScreenA = ({navigation}) => {
-
-   const onPressHandler = () => {
-      navigation.navigate('Screen_B')
-   }
-   
-   return (
-      <View style={styles.body} >
-         <Text style={styles.text} >
-         Screen A
-         </Text>
-         <Pressable onPress={onPressHandler} style={({ pressed }) => ({ backgroundColor: pressed ? 'violet' : '#0f0' }) } >
-            <Text style={styles.text} >
-               Go to Screen B
-               </Text>
-
-            </Pressable>
-            </View>
-   )
-} */
-
-/* const ScreenB = ({navigation}) => {
-   
-   const onPressHandler = () => {
-      //navigation.navigate('Screen_A')
-      navigation.goBack()
-   }
-
-   return (
-      <View style={styles.body} >
-         <Text style={styles.text} >
-            Screen B
-         </Text>
-         <Pressable onPress={onPressHandler} style={({ pressed }) => ({ backgroundColor: pressed ? 'yellow' : '#0f0' }) } >
-         <Text style={styles.text} >
-               Go back to Screen A
-            </Text>
-            
-         </Pressable>
-         </View>
-      )
-   } */
-   
-   /* const styles = StyleSheet.create({
-      body: {
-         flex: 1,
-         justifyContent: 'center',
-         alignItems: 'center',
-      },
-      text: {
-         fontSize: 40,
-         fontWeight: 'bold',
-         margin: 10,
-         color: 'black'
-      }
-      
-   }) */
-
-   
-   
-   
-
-   
-   
-   
-   
-   
-   
