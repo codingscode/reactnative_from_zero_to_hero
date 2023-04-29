47e7e8787e486a265217c96abf54f86ee092b78b
codingscode <heronnestg@hotmail.com>
2023-04-29 10:12:26 -0300

    parte 126

--- rntour/src/App.js
+++ rntour/src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { enableScreens } from 'react-native-screens'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 import ScreenA from './ScreenA'
 import ScreenB from './ScreenB'
@@ -13,6 +14,17 @@ enableScreens()
 
 const Drawer = createDrawerNavigator()
 
+const icondb = ({focused}) => {
+   return (
+      <FontAwesome5 name='btc' size={focused ? 25 : 20} color={focused ? 'red' : 'lightgreen'} />
+   )
+}
+
+const iconda = ({focused}) => {
+   return (
+      <FontAwesome5 name='btc' size={focused ? 25 : 20} color={focused ? 'yellow' : 'violet'} />
+   )
+}
 
 function App() {
 
@@ -21,8 +33,8 @@ function App() {
    return (
       <NavigationContainer>
          <Drawer.Navigator initialRouteName='Screen_A' drawerPosition="right" drawerType="permanent" edgeWidth={500} hideStatusBar={true} overlayColor='red' drawerStyle={{ backgroundColor: 'pink', width: 250 }} >
-            <Drawer.Screen name="Screen_A" component={ScreenA} options={{ title: 'Screen_A Title' }} />
-            <Drawer.Screen name="Screen_B" component={ScreenB} options={{ title: 'Screen_B Title' }} />
+            <Drawer.Screen name="Screen_A" component={ScreenA} options={{ title: 'Screen_A Title', drawerIcon: iconda }} />
+            <Drawer.Screen name="Screen_B" component={ScreenB} options={{ title: 'Screen_B Title', drawerIcon: icondb }} />
          </Drawer.Navigator>
       </NavigationContainer>
    )
