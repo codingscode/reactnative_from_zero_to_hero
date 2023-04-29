13599027eae3792e36a5fe59d6538376ccced9e2
codingscode <heronnestg@hotmail.com>
2023-04-29 09:17:18 -0300

    parte 120

--- rntour/src/App.js
+++ rntour/src/App.js
@@ -1,11 +1,9 @@
-
-import React, { useState } from 'react'
+import 'react-native-gesture-handler'
+import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { enableScreens } from 'react-native-screens'
-//import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import ScreenA from './ScreenA'
 import ScreenB from './ScreenB'
@@ -13,44 +11,19 @@ enableScreens()
 
 
 
-//const Tab = createBottomTabNavigator()
-//const Tab = createMaterialBottomTabNavigator()
-const Tab = createMaterialTopTabNavigator()
+const Drawer = createDrawerNavigator()
 
 
 function App() {
 
-   const handler = ({route}) => {
-      return {
-         tabBarIcon: ({focused, size, color}) => {
-            let iconName
-            if (route.name === 'Screen_A') {
-               iconName = 'autoprefixer'
-               size = focused ? 25 : 20
-               //color = focused ? '#ff8080' : '#00e6e6'
-            }
-            else if (route.name === 'Screen_B') {
-               iconName = 'btc'
-               size = focused ? 25 : 20
-               //color = focused ? '#00e6ac' : '#cc00cc'
-            }
-            return (
-               <FontAwesome5 name={iconName} size={size} color={color} />
-            )
-         }
-         
-      }
-   }
-   
-   const tint = { activeTintColor: '#f0f', inactiveTintColor: '#33ffcc', activeBackgroundColor: '#ffff66', inactiveBackgroundColor: '#808000', showLabel: true, labelStyle: { fontSize: 14 }, showIcon: true }
    
 
    return (
       <NavigationContainer>
-         <Tab.Navigator screenOptions={handler}  tabBarOptions={tint}  activeColor='#ff6600' inactiveColor='#8080ff' barStyle={{backgroundColor: '#ffff80'}}  >
-            <Tab.Screen name="Screen_A" component={ScreenA} /* options={{ tabBarBadge: 3 }} */ />
-            <Tab.Screen name="Screen_B" component={ScreenB} />
-         </Tab.Navigator>
+         <Drawer.Navigator >
+            <Drawer.Screen name="Screen_A" component={ScreenA} />
+            <Drawer.Screen name="Screen_B" component={ScreenB} />
+         </Drawer.Navigator>
       </NavigationContainer>
    )
    
@@ -58,5 +31,4 @@ function App() {
 
 
 export default App
-
 
