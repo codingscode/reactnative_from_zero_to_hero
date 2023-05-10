00a812d7e0f900ab39d2d4214a0949703d2974d2
codingscode <heronnestg@hotmail.com>
2023-05-09 23:23:31 -0300

    parte 143

--- rntour/src/screens/Login.js
+++ rntour/src/screens/Login.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image, TextInput } from 'react-native'
 
 
 
@@ -8,7 +8,9 @@ const Login = () => {
 
    return (
       <View style={styles.body} >
-         
+         <Image style={styles.logo} source={require('../../assets/asyncstorage.png')} />
+         <Text style={styles.text} >Async Storage</Text>
+         <TextInput style={styles.input} />
       </View>
    )
 }
@@ -18,7 +20,28 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       backgroundColor: '#0080ff'
+   },
+   logo: {
+      width: 100,
+      height: 100,
+      margin: 20
+   },
+   text: {
+      fontSize: 30,
+      color: '#ffffff'
+   },
+   input: {
+      width: 300,
+      borderWidth: 1,
+      borderColor: '#555',
+      borderRadius: 10,
+      backgroundColor: '#ffffff',
+      textAlign: 'center',
+      fontSize: 20,
+      marginTop: 130,
+      marginBottom: 10
    }
+   
 })
 
 export default Login
